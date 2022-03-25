a2584c67e2f5309ad38f128dbe7d6d99d77fec0c
GraceUmutesi <52698478+GraceUmutesi@users.noreply.github.com>
2022-03-25 22:24:07 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+ 
+  let taskList = new TaskList();
+  let newTaskForm = document.getElementById("create-task-form");
+  let newTaskDescription = document.getElementById("new-task-description");
+  let newTaskPriority = document.getElementById("new-task-priority");
+  let taskUl = document.getElementById("tasks");
+  let renderApp = () => (taskUl.innerHTML = taskList.renderTasks());
+
+
+  newTaskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    taskList.createNewTask(newTaskDescription.value);
+    e.target.reset();
+    renderApp();
+  });
+
+  taskUl.addEventListener("click", (e) => {
+    if (e.target.nodeName === "BUTTON") {
+      taskList.deleteTask(e.target.dataset.description);
+      renderApp();
+    }
+  });
 });
